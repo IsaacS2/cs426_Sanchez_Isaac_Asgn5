1b4e1801b5695836dc36c32d6032627092274bd7
aruyy3 <111813092+aruyy3@users.noreply.github.com>
2024-04-07 18:58:49 -0500

    first sound effect done (win sound)

--- Assets/Scripts/FaceColider.cs
+++ Assets/Scripts/FaceColider.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class FaceCollider : MonoBehaviour
 {
+    public AudioSource soundEffect; // Reference to the AudioSource component for the sound effect
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            // Send message to trigger terrified animation
             transform.parent.SendMessage("TriggerTerrifiedAnimation");
+
+            // Play the sound effect
+            if (soundEffect != null)
+            {
+                soundEffect.Play();
+            }
         }
     }
 }
